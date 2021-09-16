61870a8f54bcb396ac58ddb322fd82b38f0ef72b
AbadiHAK <abdulelahcypercompany@gmail.com>
2021-09-16 13:36:40 +0300

    make it better

--- healthCareProject/settings.py
+++ healthCareProject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
 
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,6 +132,4 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-DATABASES = { 'default': dj_database_url.config() }
